1ecaa0cd0f309396a63eeafebab088b8178e64e3
Yifan Wu <tyivanwu@gmail.com>
2017-07-15 15:55:10 -0400

    prevent open internal links in Safari

--- js/subjpop.js
+++ js/subjpop.js
@@ -8,17 +8,25 @@ function openInNewTab() {
     $('a[href^="https://"]').attr('target', '_blank')
 }
 
-function escapeRegExp(string) {
-    return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
-}
+// Mobile Safari in standalone mode
+if (("standalone" in window.navigator) && window.navigator.standalone) {
 
-if (window.navigator.standalone) {
-    var local = document.domain;
-    $(document).on('click', 'a', function (event) {
-        var a = $(this).attr('href');
-        if (a.match('https?://(www\\.)?' + escapeRegExp(local))) {
-            event.preventDefault();
-            document.location.href = a;
+    // If you want to prevent remote links in standalone web apps opening Mobile Safari, change 'remotes' to true
+    var noddy, remotes = true;
+
+    document.addEventListener('click', function (event) {
+
+        noddy = event.target;
+
+        // Bubble up until we hit link or top HTML element. Warning: BODY element is not compulsory so better to stop on HTML
+        while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+            noddy = noddy.parentNode;
         }
-    });
+
+        if ('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
+            event.preventDefault();
+            document.location.href = noddy.href;
+        }
+
+    }, false);
 }
