c5d6d6e8bac0f37a3e1e9e1559335c0a428ded40
Yifan Wu <tyivanwu@gmail.com>
2017-09-22 08:58:42 -0400

    add post 34

---
+++ rakefile.rb
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/3668607/how-to-excute-commands-within-rake-tasks
+task(:dev) do
+  puts "start serve watch"
+  sh('open http://127.0.0.1:4000/')
+  sh('bundle exec jekyll serve --watch --limit_posts 1')
+end
+
+task(:build) do
+  puts "start building"
+  sh('subjpop')
+end
+
