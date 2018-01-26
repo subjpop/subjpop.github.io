551d028ee8983c5f8b5baf626284f314c4826ba5
Yifan Wu <tyivanwu@gmail.com>
2018-01-26 19:47:50 +0800

    add post 38

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@
 task(:dev) do
   puts "start serve watch"
   sh('open http://127.0.0.1:4000/')
-  sh('bundle exec jekyll serve --watch --limit_posts 1')
+  sh('bundle exec jekyll serve --livereload --limit_posts 1')
 end
 
 task(:build) do
