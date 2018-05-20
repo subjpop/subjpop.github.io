50dd1d099a9ba8c76459edcda46ee4dcd5ac2923
Yifan Wu <tyivanwu@gmail.com>
2018-05-19 22:12:45 -0400

    🎊 subjpop.com updated!

--- js/subjpop.js
+++ js/subjpop.js
@@ -7,13 +7,3 @@ function openInNewTab() {
     $('a[href^="http://"]').attr('target', '_blank').attr('rel', 'noopener')
     $('a[href^="https://"]').attr('target', '_blank').attr('rel', 'noopener')
 }
-
-// lazy load offscreen img
-(function (w, d) {
-    var b = d.getElementsByTagName('body')[0];
-    var s = d.createElement("script"); s.async = true;
-    var v = !("IntersectionObserver" in w) ? "8.7.1" : "10.5.2";
-    s.src = "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/" + v + "/lazyload.min.js";
-    w.lazyLoadOptions = {}; // Your options here. See "recipes" for more information about async.
-    b.appendChild(s);
-}(window, document));
