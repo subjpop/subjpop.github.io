1b61c6f8659b20ba134efac9dd1cf4546e73c7f4
Yifan Wu <tyivanwu@gmail.com>
2018-10-28 23:03:18 -0400

    🎊 subjpop.com updated!

--- js/subjpop.js
+++ js/subjpop.js
@@ -1,9 +1,28 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     openInNewTab();
+    markLatest();
 });
 
 // open external link in a new tab
-function openInNewTab() {
-    $('a[href^="http://"]').attr('target', '_blank').attr('rel', 'noopener')
-    $('a[href^="https://"]').attr('target', '_blank').attr('rel', 'noopener')
+const openInNewTab = () => {
+    document.querySelectorAll('a').forEach(function (a) {
+        if (! a.href.startsWith(document.location.origin)) {
+            a.setAttribute('target', '_blank');
+            a.setAttribute("rel", "noopener");
+        }
+    })
+    // $('a[href^="http://"]').attr('target', '_blank').attr('rel', 'noopener')
+    // $('a[href^="https://"]').attr('target', '_blank').attr('rel', 'noopener')
 }
+
+const markLatest = () => {
+    const allDates = document.querySelectorAll("time");
+    const days = 30;
+    // days => ms
+    const criteria = days * 24 * 60 * 60 * 1000;
+    allDates.forEach(function(element) {
+        if ((Date.now() - Date.parse(element.dateTime)) < criteria) {
+            element.parentElement.parentElement.classList.add("latest");
+        }
+    });
+};
